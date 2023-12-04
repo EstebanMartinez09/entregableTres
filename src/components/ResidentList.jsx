ec1e9a2b9d1ab1cc85cf7684d04dbd04309ebffb
Esteban Martinez <camilo.martinezmma@cun.edu.co>
2023-12-04 18:56:46 -0500

    src

--- src/components/ResidentList.jsx
+++ src/components/ResidentList.jsx
@@ -1,6 +1,7 @@
 import { useEffect, useState } from "react";
 import { paginationLogic } from "../utils/pagination";
-import { ResidentCard } from "./residentCard";
+import { ResidentCard } from "./ResidentCard2222";
+
 
 export const ResidentList = ({ residents }) => {
   const [currentPage, setCurrentPage] = useState(1);
@@ -21,7 +22,7 @@ export const ResidentList = ({ residents }) => {
     <>
       <section className="my-10  grid gap-8 grid-cols-[repeat(auto-fill,_250px)] justify-center max-w-[1200px] mx-auto ">
         {residentsInCurrentPage.map((resident) => (
-          <ResidentCard residentURL={resident} key={resident} />
+            <ResidentCard residentURL={resident} key={resident} />
         ))}
       </section>
       {/* pagination */}
